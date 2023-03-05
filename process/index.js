b45cb71e1856b7091c7b242a39025a27c6a2dc50
Team 1280 Programming Laptop <59985235+Team1280Programming@users.noreply.github.com>
2023-03-05 15:58:33 -0800

    Add CSV processing

---
+++ process/index.js
@@ -0,0 +1,44 @@
+const textarea = document.getElementById('pastebin')
+const output = document.getElementById('out')
+
+const convert = () => {
+    const data = textarea.value
+    const entries = data.split('\n')
+    let cols = new Set();
+
+    for (let entry of entries) {
+        const fields = entry.split(';')
+        for (let field of fields) {
+            const col = field.split('=')[0]
+            cols.add(col)
+        }
+    }
+
+    cols = Array.from(cols)
+    let list = []
+    for (let entry of entries) {
+        let entryData = []
+        const fields = entry.split(';')
+        let fieldNames = fields.map(f => f.split('=')[0])
+
+        for (let col of cols) {
+            if (fieldNames.includes(col)) {
+                const field = fields[fieldNames.indexOf(col)]
+                const value = field.split('=')[1]
+                entryData.push(value)
+            } else {
+                entryData.push(null)
+            }
+        }
+
+        list.push(entryData)
+    }
+
+    let csv = cols.toString() + '\n'
+    for (let entry of list) {
+        csv += entry.toString() + '\n'
+    }
+    output.innerText = csv
+}
+
+const copy = () => navigator.clipboard.writeText(out.innerText)
